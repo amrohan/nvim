3817fff62d2e8f78ed318a37f7326f5b9953795f
amrohan <rohanxsalunkhe@gmail.com>
2025-03-29 10:40:04 +0530

    add buffer switch with H and L

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -47,6 +47,8 @@ return {
         -- second key is the lefthand side of the map
 
         -- navigate buffer tabs
+        ["H"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+        ["L"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
         ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
         ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
 
